3d98a3da1ddb8afe035704fde47bad75aa4a4a80
PaolaBronza <125701556+PaolaBronza@users.noreply.github.com>
2023-03-16 18:45:17 -0300

    Rename Atividade sala 05.cpp to Atividade sala 05.c

--- Atividade sala 05.c
+++ Atividade sala 05.c
@@ -6,16 +6,16 @@
 int main(void){
 	setlocale(LC_ALL,"portuguese");
 	float num,num2,num3;
-	printf("insira o preço do seu produto: ");
+	printf("insira o preÃ§o do seu produto: ");
 	scanf("%f",&num);
 	
 	if(num>=100){
 		num2=num-(num*0.1);
 		printf("\nSeu produto recebeu desconto de 10%");
-		printf("\nPreço inicial: %f\nPreço com desconto: %f",num,num2);
+		printf("\nPreÃ§o inicial: %f\nPreÃ§o com desconto: %f",num,num2);
 	}
 	else if(num<99){
-		printf("\nSeu produto não recebeu desconto.");
+		printf("\nSeu produto nÃ£o recebeu desconto.");
 	}
 	return 0;
 }
